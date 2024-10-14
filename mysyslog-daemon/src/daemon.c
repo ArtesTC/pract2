386a9766bfb471e8a0e776f5f6eaab88a4874809
Artes <124509803+ArtesTC@users.noreply.github.com>
2024-10-14 23:06:33 +0300

    Update daemon.c

--- mysyslog-daemon/src/daemon.c
+++ mysyslog-daemon/src/daemon.c
@@ -1,24 +1,24 @@
-// daemon.c
 #include <stdio.h>
 #include <stdlib.h>
-#include <signal.h>
 #include <unistd.h>
-#include "mysyslog.h"
+#include <signal.h>
+#include <syslog.h>
+#include "../libmysyslog.h"
 
-void signal_handler(int sig) {
-    if (sig == SIGTERM) {
-        mysyslog("Daemon stopped", 4, 0, FORMAT_TEXT, "daemon.log");
-        exit(0);
-    }
+void signal_handler(int signum) {
+    syslog(LOG_INFO, "Daemon shutting down.");
+    exit(0);
 }
 
 int main() {
     signal(SIGTERM, signal_handler);
-
+    openlog("mysyslog-daemon", LOG_PID|LOG_CONS, LOG_USER);
+    
     while (1) {
-        mysyslog("Daemon running", 1, 0, FORMAT_TEXT, "daemon.log");
-        sleep(10);
+        syslog(LOG_INFO, "Daemon is running.");
+        sleep(10); // логировать каждые 10 секунд
     }
 
+    closelog();
     return 0;
 }
