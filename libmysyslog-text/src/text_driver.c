57f32ace7b99d3bb8e3638710bd121595a078531
Artes <124509803+ArtesTC@users.noreply.github.com>
2024-10-15 22:34:06 +0300

    Delete libmysyslog-text directory

--- libmysyslog-text/src/text_driver.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <time.h>
-#include "../libmysyslog.h"
-
-// Функция для логирования в текстовом формате
-void log_to_text(const char* msg, int level, const char* path) {
-    mysyslog(msg, level, DRIVER_TEXT, TEXT, path);
-}
