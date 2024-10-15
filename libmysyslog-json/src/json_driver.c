a9f6caddbbb64ec1cc808121a5b6815a9c354590
Artes <124509803+ArtesTC@users.noreply.github.com>
2024-10-15 22:34:00 +0300

    Delete libmysyslog-json directory

--- libmysyslog-json/src/json_driver.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <time.h>
-#include "../libmysyslog.h"
-
-// Функция для логирования в формате JSON
-void log_to_json(const char* msg, int level, const char* path) {
-    mysyslog(msg, level, DRIVER_JSON, JSON, path);
-}
